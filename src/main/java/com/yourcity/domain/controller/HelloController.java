289341eeffdbf4f9a3d245b4146fb31ed3a1126b
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2024-05-23 15:39:48 +0300

    feat: create a RestController

--- src/main/java/com/yourcity/domain/controller/HelloController.java
+++ src/main/java/com/yourcity/domain/controller/HelloController.java
@@ -2,12 +2,13 @@ package com.yourcity.domain.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "/hello";
+        return "Hello world out of REST controller!!!";
     }
 }
